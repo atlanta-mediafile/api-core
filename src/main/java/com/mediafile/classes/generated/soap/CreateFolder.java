709ded5c707bd09863e70601773ae329005b48b7
diegomated1 <diegocardwr@gmail.com>
2024-03-02 11:24:35 -0500

    chore: soap and grpc generated classes

---
+++ src/main/java/com/mediafile/classes/generated/soap/CreateFolder.java
@@ -0,0 +1,125 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2024.03.02 a las 10:58:02 AM COT 
+//
+
+
+package com.mediafile.classes.generated.soap;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para CreateFolder complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="CreateFolder"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="target" type="{http://www.generated.classes.mediafile.com/soap}Identifiers"/&gt;
+ *         &lt;element name="folderId" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "CreateFolder", propOrder = {
+    "target",
+    "folderId",
+    "name"
+})
+public class CreateFolder {
+
+    @XmlElement(required = true)
+    protected Identifiers target;
+    @XmlElement(required = true)
+    protected String folderId;
+    @XmlElement(required = true)
+    protected String name;
+
+    /**
+     * Obtiene el valor de la propiedad target.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Identifiers }
+     *     
+     */
+    public Identifiers getTarget() {
+        return target;
+    }
+
+    /**
+     * Define el valor de la propiedad target.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Identifiers }
+     *     
+     */
+    public void setTarget(Identifiers value) {
+        this.target = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad folderId.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFolderId() {
+        return folderId;
+    }
+
+    /**
+     * Define el valor de la propiedad folderId.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFolderId(String value) {
+        this.folderId = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad name.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Define el valor de la propiedad name.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+}
