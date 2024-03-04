6c7df00540f47657dc7452784ae0c17c003f421e
diegomated1 <diegocardwr@gmail.com>
2024-03-03 21:43:32 -0600

    chore: update soap models

---
+++ src/main/java/com/mediafile/classes/generated/soap/Identifiers.java
@@ -0,0 +1,125 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2024.03.03 a las 09:04:57 PM CST 
+//
+
+
+package com.mediafile.classes.generated.soap;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para Identifiers complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="Identifiers"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="userId" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="folderId" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="fileId" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Identifiers", propOrder = {
+    "userId",
+    "folderId",
+    "fileId"
+})
+public class Identifiers {
+
+    @XmlElement(required = true)
+    protected String userId;
+    @XmlElement(required = true)
+    protected String folderId;
+    @XmlElement(required = true)
+    protected String fileId;
+
+    /**
+     * Obtiene el valor de la propiedad userId.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     * Define el valor de la propiedad userId.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUserId(String value) {
+        this.userId = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad folderId.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFolderId() {
+        return folderId;
+    }
+
+    /**
+     * Define el valor de la propiedad folderId.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFolderId(String value) {
+        this.folderId = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad fileId.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFileId() {
+        return fileId;
+    }
+
+    /**
+     * Define el valor de la propiedad fileId.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFileId(String value) {
+        this.fileId = value;
+    }
+
+}
