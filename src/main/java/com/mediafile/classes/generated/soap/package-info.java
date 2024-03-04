1089058039fb6e02ef6c963a8664b9c7f40641e7
diegomated1 <diegocardwr@gmail.com>
2024-03-03 21:40:14 -0600

    chore: add username to getUserResponse

--- src/main/java/com/mediafile/classes/generated/soap/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
-// Visite https://eclipse-ee4j.github.io/jaxb-ri 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.03.02 a las 02:38:04 PM COT 
-//
-
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.generated.classes.mediafile.com/soap", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.mediafile.classes.generated.soap;
