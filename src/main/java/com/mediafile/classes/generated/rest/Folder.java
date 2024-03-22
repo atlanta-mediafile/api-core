610a1fb8e506c2d95d908cd5f921136b53fb0b7e
diegomated1 <diegocardwr@gmail.com>
2024-03-21 20:42:09 -0600

    chore: add status, parentId and createdDate atributes

--- src/main/java/com/mediafile/classes/generated/rest/Folder.java
+++ src/main/java/com/mediafile/classes/generated/rest/Folder.java
@@ -12,8 +12,10 @@ public class Folder {
     
     private String id;
     private String name;
-    private String folderId;
-
+    private boolean status;
+    private String parentId;
+    private long createdDate;
+    
     public String getId() {
         return id;
     }
@@ -22,10 +24,28 @@ public class Folder {
         return name;
     }
 
-    public String getFolderId() {
-        return folderId;
+    public String getParentId() {
+        return parentId;
     }
 
+    public long getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(int createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    
+    
     public void setId(String id) {
         this.id = id;
     }
@@ -34,8 +54,13 @@ public class Folder {
         this.name = name;
     }
 
-    public void setFolderId(String folderId) {
-        this.folderId = folderId;
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Folder{" + "id=" + id + ", name=" + name + ", createdDate=" + createdDate + ", status=" + status + ", parentId=" + parentId + '}';
     }
     
 }
